19f690a29d19c847b30860cd8b3ca164d961c402
Laura Martinez <lmartinez@jumpmind.com>
2025-05-06 17:38:45 -0400

    project created

---
+++ SideTest/src/main/java/com/example/sidetest/SideTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sidetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideTestApplication.class, args);
+    }
+
+}
---
+++ SideTest/src/test/java/com/example/sidetest/SideTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
